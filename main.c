9e67ae3cf910e4a13b42dc16beb3db2f6b0f8b4b
Ryan Rodrigues <ryanpimentelbr@gmail.com>
2024-09-21 18:58:39 -0300

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,12 @@
 
 int main() {
     // Ler String
-    char string[] = "AM_25 E1_04_S_6,9,8  E2_11_D_5,8,9,3,2,T E3_20_D_5,8,T,9 T5_P01_S_4_6 T8_P01_D_6_4 T9_P02_S_5_10 T10_P03_S_3_9 T10_P04_D_6_T T10_P05_S_8_15 T15_P06_D_9_2 T15_P07_S_2_13 T18_P08_D_8_T T21_P01_D_16_3 T21_P10_S_T_13 T21_P11_S_T_12 T23_P12_S_T_15 T28_P13_S_2_13";
+    // char string[] = "AM_25 E1_04_S_6,9,8  E2_11_D_5,8,9,3,2,T E3_20_D_5,8,T,9 T5_P01_S_4_6 T8_P01_D_6_4 T9_P02_S_5_10 T10_P03_S_3_9 T10_P04_D_6_T T10_P05_S_8_15 T15_P06_D_9_2 T15_P07_S_2_13 T18_P08_D_8_T T21_P01_D_16_3 T21_P10_S_T_13 T21_P11_S_T_12 T23_P12_S_T_15 T28_P13_S_2_13";
+    printf("Digite a string de entrada: ");
+    char entrada[10000];
+    fgets(entrada, 10000, stdin);
+    char string[strlen(entrada) + 1];
+    strcpy(string, entrada);
     char *ps = string;
     int n = atoi(&ps[3]); // numero de andares
     int m = 0;
@@ -59,7 +64,7 @@ int main() {
     }
         
 
-    printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-\n");
+    printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-\n\n");
     mostrar_rotas(elevadores, m);
     printf("\nTempo Total: %ld segundos\n", tempo);
     printf("Energia Total consumida: %d", energia);
